bd899094b3a0bd820a1b0c6a07e78b98e3568bfb
angelikeholy <cnjonathan@yahoo.com>
2018-11-25 23:36:12 +0700

    done

---
+++ application/views/welcome_message.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html>
+<html lang="zxx" class="no-js">
+<head>
+  <!-- Mobile Specific Meta -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!-- Favicon-->
+  <link rel="shortcut icon" href="image/icon.jpg">
+  <!-- Author Meta -->
+  <meta name="author" content="CodePixar">
+  <!-- Meta Description -->
+  <meta name="description" content="">
+  <!-- Meta Keyword -->
+  <meta name="keywords" content="">
+  <!-- meta character set -->
+  <meta charset="UTF-8">
+  <!-- Site Title -->
+  <title>Angelike Holy</title>
+
+  <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet">
+    <!--
+    CSS
+    ============================================= -->
+    <link rel="stylesheet" href="master/css/linearicons.css">
+    <link rel="stylesheet" href="master/css/font-awesome.min.css">
+    <link rel="stylesheet" href="master/css/magnific-popup.css">
+    <link rel="stylesheet" href="master/css/nice-select.css">
+    <link rel="stylesheet" href="master/css/owl.carousel.css">
+    <link rel='stylesheet' href='master/css/simplelightbox.min.css' >
+    <link rel="stylesheet" href="master/css/bootstrap.css">
+    <link rel="stylesheet" href="master/css/main.css">
+
+  </head>
+  <body>
+    <!-- Start Header Area -->
+    <header class="default-header">
+      <div class="container">
+        <div class="header-wrap">
+          <div class="header-top d-flex justify-content-between align-items-center">
+            <div class="logo">
+              <a href="#home"><img src="image/logo.png" alt=""></a>
+            </div>
+            <div class="main-menubar d-flex align-items-center">
+              <nav class="hide">
+                <a href="#home">Angelike</a>
+                <a href="#about">About Me</a>
+                <a href="#gallery">Gallery</a>
+                <a href="#contact">Contact</a>
+              </nav>
+              <div class="menu-bar"><span class="lnr lnr-menu"></span></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+    <!-- End Header Area -->
+    <!-- start banner Area -->
+    <section class="banner-area relative" id="home">
+      <div class="slider"><div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+              <div class="carousel-inner" role="listbox">
+                <!-- Slide One - Set the background image for this slide in the line below -->
+                <div class="carousel-item active" style="background-image: url('image/4.jpg')">
+                  <div class="carousel-caption d-md-block">
+                    <h2 class="text-uppercase">Oiiiiiiiii....</h2>
+                    <p>
+              Happy birthday to you!!!
+                    </p>
+                  </div>
+                </div>
+                <!-- Slide Two - Set the background image for this slide in the line below -->
+                <div class="carousel-item" style="background-image: url('image/2.jpg')">
+                  <div class="carousel-caption d-md-block">
+                    <h2 class="text-uppercase">Mazmur 20:4</h2>
+                    <p>
+              Kiranya diberikan-Nya kepadamu apa yang kaukehendaki<br> dan dijadikan-Nya berhasil apa yang kau rancangkan.<br>
+              <span><i>May he give you the desire of your heart and make all your plans succeed. </i></span>
+
+                    </p>
+                  </div>
+                </div>
+                <!-- Slide Three - Set the background image for this slide in the line below -->
+                <div class="carousel-item" style="background-image: url('image/3.jpg')">
+                  <div class="carousel-caption d-md-block">
+                    <h2 class="text-uppercase">Wish you all the best</h2>
+                    <p>
+              Tuhan berkatimu...
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+              </a>
+              <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+              </a>
+            </div>
+      </div>
+    </section>
+    <!-- End banner Area -->
+
+
+    <!-- Start About Area -->
+    <section class="About-area section-gap" id="about">
+      <div class="container">
+        <div class="row d-flex align-items-center">
+          <div class="col-lg-6 about-left">
+            <img class="img-fluid" src="image/1.jpg" alt="">
+          </div>
+          <div class="col-lg-6 about-right">
+            <h1>
+              About you....
+            </h1>
+            <p>
+              Angelike, yang ulang tahun pada hari ini, tanggal 25 November tahunnya lupa. Sukanya Beto, anjingnya banyak. Terkenal cuek bagi yang tidak mengenalnya. Lek kadung kenal, diluar ekspektasi. Dibilang lakik, tapi gak. Talentnya banyak, bisa nggitar, main piano, bass juga tapi rodok grotal gratul, nyanyinya oke juga. Seenggaknya mau belajar, itu yang bikin keren. Berani meskipun belum bisa. Paling master bidang editing video. Juga master bikin kue (Jar of Flour). Harapanku, jadilah berkat dengan semua talentmu, lebih dipakai Tuhan lagi untuk melayaniNya. ~baperistique
+            </p>
+            <a href="https://www.instagram.com/angelikeholy/" role="button" class="submit-btn primary-btn mt-20 text-uppercase ">Hire me now<span class="lnr lnr-arrow-right"></span></button>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- End About Area -->
+
+    <!-- Start gallery Area -->
+    <section class="gallery-area section-gap" id="gallery">
+      <div class="container">
+        <div class="gal">
+          <a href="image/0.jpg"><img src="image/0.jpg" alt=""></a>
+          <a href="image/1.jpg"><img src="image/1.jpg" alt=""></a>
+          <a href="image/2.jpg"><img src="image/2.jpg" alt=""></a>
+          <a href="image/3.jpg"><img src="image/3.jpg" alt=""></a>
+          <a href="image/4.jpg"><img src="image/4.jpg" alt=""></a>
+          <a href="image/5.jpg"><img src="image/5.jpg" alt=""></a>
+          <a href="image/6.jpg"><img src="image/6.jpg" alt=""></a>
+          <a href="image/7.jpg"><img src="image/7.jpg" alt=""></a>
+          <a href="image/8.jpg"><img src="image/8.jpg" alt=""></a>
+          <a href="image/9.jpg"><img src="image/9.jpg" alt=""></a>
+          <a href="image/10.jpg"><img src="image/10.jpg" alt=""></a>
+          <a href="image/11.jpg"><img src="image/11.jpg" alt=""></a>
+          <a href="image/12.jpg"><img src="image/12.jpg" alt=""></a>
+          <a href="image/13.jpg"><img src="image/13.jpg" alt=""></a>
+          <a href="image/14.jpg"><img src="image/14.jpg" alt=""></a>
+          <a href="image/15.jpg"><img src="image/15.jpg" alt=""></a>
+          <a href="image/16.jpg"><img src="image/16.jpg" alt=""></a>
+          <a href="image/17.jpg"><img src="image/17.jpg" alt=""></a>
+        </div>
+      </div>
+    </section>
+
+    <!-- Start contact Area -->
+    <section class="contact-area" id="contact">
+      <div class="container-fluid">
+        <div class="row d-flex justify-content-end align-items-center">
+          <div class="col-lg-5 col-md-12 contact-left no-padding">
+            <img class="img-fluid" src="image/15.jpg" alt="">
+          </div>
+          <div class="col-lg-7 col-md-12 contact-right no-padding">
+            <h1>Sorry telat wkwkwkwk...</h1>
+            <p>
+
+            </p>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- End contact Area -->
+
+    <!-- start footer Area -->
+    <footer class="footer-area">
+      <div class="container">
+        <div class="row footer-bottom d-flex justify-content-between">
+          <p class="col-lg-8 col-sm-12 footer-text m-0 text-white">Copyright © 2017 All rights reserved   |   This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="#">Colorlib</a><br>Developed by <a href="https://www.instagram.com/cnjonathan/">Christopher Nanda Jonathan</a><br>25 November 2018</p>
+        </div>
+      </div>
+    </footer>
+    <!-- End footer Area -->
+
+    <script src="master/js/vendor/jquery-2.2.4.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="master/js/vendor/bootstrap.min.js"></script>
+    <script src="master/js/jquery.ajaxchimp.min.js"></script>
+    <script src="master/js/owl.carousel.min.js"></script>
+    <script src="master/js/jquery.nice-select.min.js"></script>
+    <script src="master/js/jquery.sticky.js"></script>
+    <script src="master/js/parallax.min.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script type="text/javascript" src="master/js/simple-lightbox.min.js"></script>
+    <script src="master/js/main.js"></script>
+  </body>
+</html>
